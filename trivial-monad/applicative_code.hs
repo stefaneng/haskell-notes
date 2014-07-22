de8fd670581355c95ddccab3ed5882aeb2031c24
Stefan Eng <stefaneng13@gmail.com>
2014-07-22 14:51:04 -0700

    Trival Monad: Add two exercises to replace #4

--- trivial-monad/applicative_code.hs
+++ trivial-monad/applicative_code.hs
@@ -18,3 +18,13 @@ g x y = pure (+ x) <*> y
 -- Exercise 2:
 h :: W Int -> W Int -> W Int
 h x y = pure (+) <*> x <*> y
+
+-- Exercise 4:
+-- | Sequence actions, discarding the value of the first argument.
+(*>) :: (Applicative f ) => f a -> f b -> f b
+x *> y = pure (const id) <*> x <*> y
+
+
+-- | Sequence actions, discarding the value of the second argument.
+(<*) :: (Applicative f) => f a -> f b -> f a
+x <* y = pure const <*> x <*> y
